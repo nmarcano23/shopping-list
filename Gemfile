c295192344731bec755e373d616d664df74bdb43
Nelson Marcano <nerumarcano@gmail.com>
2019-05-26 18:07:47 -0300

    list bootstrap finished

--- Gemfile
+++ Gemfile
@@ -61,3 +61,11 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "bootstrap-generators", "~> 3.3"
+
+gem "haml-rails", "~> 2.0"
+
+gem "annotate", "~> 2.7"
+
+gem "jquery-rails", "~> 4.3"
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module ShoppingList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.load_defaults 5.2
+    config.i18n.default_locale = :es
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'env_variables.yml')
       YAML.load(File.open(env_file))[Rails.env].each do |key, value|
